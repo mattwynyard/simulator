871c6536ad805ee6cd37bbfd95e334a546b30fce
mjwynyard@gmail.com <mjwynyard@gmail.com>
2022-05-20 08:10:35 +1200

    plots fault lines

---
+++ client/src/Util.js
@@ -0,0 +1,49 @@
+export const getPointRadius = (zoom) => {
+    switch (zoom) {
+        case 18:
+            return 9;
+        case 17:
+            return 8;
+        case 16:
+            return 7;
+        case 15:
+            return 6;
+        case 14:
+            return 4;
+        case 13:
+            return 3;
+        default:
+            return 3;
+    }
+}
+
+export const getLineWeight = (zoom) => {
+    switch (zoom) {
+        case 18:
+            return 10;
+        case 17:
+            return 9;
+        case 16:
+            return 8;
+        case 15:
+            return 7;
+        case 14:
+            return 6;
+        case 13:
+            return 5;
+        default:
+            return 4;
+    }
+}
+
+export const getColor = (priority) => {
+    if (priority === 1) {
+        return "#EE00EE";
+    } else if (priority === 2) {
+        return "#DD7500";	 
+    } else if (priority === 3) {
+        return "#00CD00";
+    } else {
+        return "#000000";
+    }
+}
